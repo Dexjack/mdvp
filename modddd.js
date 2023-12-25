55deb045c5efd7a8e17c4f2dfed11b73c7c095a4
Dexjack <119853330+Dexjack@users.noreply.github.com>
2023-12-25 21:11:19 +0300

    Update modddd.js

--- modddd.js
+++ modddd.js
@@ -146,7 +146,7 @@ function(t) {
 	}
 	var TRASH_R = ['$$$####!!!!!!!', '^^^^^^##@', '@!^^!@#@@$$$$$', '^^#@@!!@#!$', '@#!@@@##$$@@'];
 	var version_modss = '3.0',
-		API = Protocol() + 'https://dexjack.github.io/mdvp/api.json',
+		API = Protocol() + 'api.lampa.stream',
 		type = '',
 		jackets = {}, cards, ping_auth, manifest, menu_list = [],
 		vip = true,
